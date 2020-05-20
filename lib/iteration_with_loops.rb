576cb36ce735ab59497c97ec2ea37f843d7c5967
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-20 14:07:15 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,10 +6,10 @@ $array_of_arrays=[
 def find_even_values(src)
  
     ind=0
-while ind<$array_of_arrays.length do
+while ind<array_of_arrays.length do
   inner_ind=0
-    while inner_ind<$array_of_arrays[ind] do
-    $array_of_arrays[ind][inner_ind].even?
+    while inner_ind<array_of_arrays[ind] do
+    array_of_arrays[ind][inner_ind].even?
     puts inner_ind
     inner_ind+=1
     end
@@ -18,4 +18,4 @@ end
 end
  
 
-find_even_values($array_of_arrays)
+find_even_values(array_of_arrays)
