f91591925657fbb0e01a2aab85dcbfdc7ee6b986
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-20 14:03:07 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,8 +1,4 @@
- array_of_arrays=[
-    [10,11],
-    [99,50,3,4],
-    [23,41]
-    ]
+
 def find_even_values(src)
  
     ind=0
@@ -16,6 +12,10 @@ while ind<array_of_arrays.length do
     ind+=1
 end 
 end
-
+ array_of_arrays=[
+    [10,11],
+    [99,50,3,4],
+    [23,41]
+    ]
 
 find_even_values(array_of_arrays)
